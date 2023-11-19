2175b04a4ee1011fe0147d4a810fa10b1a2375ba
huong03 <phamquanghuong03@gmail.com>
2023-11-19 15:38:19 +0700

    thuphuong

--- src/main/java/com/example/sd_95_polo_store_be/Controller/Admin/CategoriesController.java
+++ src/main/java/com/example/sd_95_polo_store_be/Controller/Admin/CategoriesController.java
@@ -1,10 +1,8 @@
-package com.example.sd_95_polo_store_be.Controller;
+package com.example.sd_95_polo_store_be.Controller.Admin;
 
-
-
+import com.example.sd_95_polo_store_be.Model.Entity.Categories;
 import com.example.sd_95_polo_store_be.Model.Entity.Colors;
-import com.example.sd_95_polo_store_be.Model.Entity.Sizes;
-import com.example.sd_95_polo_store_be.Service.ColorServices;
+import com.example.sd_95_polo_store_be.Service.CategoriesService;
 import com.example.sd_95_polo_store_be.common.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -16,33 +14,33 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-@RequestMapping("/color")
+@RequestMapping("/categories")
 @RestController
-public class ClolorController {
+public class CategoriesController {
     @Autowired
-    private ColorServices colorServices;
+    CategoriesService categoriesSrevice;
 
     @GetMapping("/getall")
-    public Response<List<Colors>> getAll() {
-        return Response.ofSucceeded(colorServices.getAllColor());
+    public Response<List<Categories>> getAll() {
+        return Response.ofSucceeded(categoriesSrevice.getAllCategories());
     }
 
     @PostMapping("/add")
-    public Response<Colors> create(@RequestBody Colors color) {
+    public Response<Categories> create(@RequestBody Categories categories) {
         try {
-            Colors colors = colorServices.saveColor(color);
-            return Response.ofSucceeded(colors);
+           Categories categorie = categoriesSrevice.saveCategories(categories);
+            return Response.ofSucceeded(categorie);
         } catch (IllegalArgumentException e) {
             return Response.ofError(e.getMessage());
         }
     }
 
     @PutMapping("/update/{id}")
-    public Response<Colors> update(@PathVariable Long id, @RequestBody Colors color) {
+    public Response<Categories> update(@PathVariable Long id, @RequestBody Categories categories) {
         try {
-            color.setId(id);
-            Colors colors = colorServices.saveColor(color);
-            return Response.ofSucceeded(colors);
+            categories.setId(id);
+            Categories categorie = categoriesSrevice.saveCategories(categories);
+            return Response.ofSucceeded(categorie);
         } catch (IllegalArgumentException e) {
             return Response.ofError(e.getMessage());
         }
@@ -50,20 +48,20 @@ public class ClolorController {
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> delete(@PathVariable Long id) {
-
         try {
-            colorServices.deleteColorById(id);
-            return ResponseEntity.ok("Màu sắc đã được xóa thành công");
+            categoriesSrevice.deleteCategoriesById(id);
+            return ResponseEntity.ok("Loại đã được xóa thành công");
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+
 
     }
 
     @GetMapping("/get-page")
     public List<?> getPage(@RequestParam(defaultValue = "0") int p) {
         Pageable pageable = PageRequest.of(p, 5);
-        Page<Colors> page = colorServices.findAllColor(pageable);
+        Page<Categories> page = categoriesSrevice.findAllCategories(pageable);
         return page.toList();
     }
 
@@ -71,11 +69,12 @@ public class ClolorController {
     public Response<List<Long>> deleteMultiple(@RequestBody Map<String, List<Long>> request) {
         List<Long> ids = request.get("id");
         try {
-            colorServices.deleteColorsByIds(ids);
+            categoriesSrevice.deleteCategoriesByIds(ids);
             return Response.ofSucceeded();
         } catch (IllegalArgumentException e) {
             return Response.ofError(e.getMessage());
         }
-
     }
+
+
 }
--- src/main/java/com/example/sd_95_polo_store_be/Controller/Admin/ClolorController.java
+++ src/main/java/com/example/sd_95_polo_store_be/Controller/Admin/ClolorController.java
@@ -1,8 +1,10 @@
-package com.example.sd_95_polo_store_be.Controller;
+package com.example.sd_95_polo_store_be.Controller.Admin;
 
-import com.example.sd_95_polo_store_be.Model.Entity.Categories;
+
+
 import com.example.sd_95_polo_store_be.Model.Entity.Colors;
-import com.example.sd_95_polo_store_be.Service.CategoriesService;
+import com.example.sd_95_polo_store_be.Model.Entity.Sizes;
+import com.example.sd_95_polo_store_be.Service.ColorServices;
 import com.example.sd_95_polo_store_be.common.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -14,33 +16,33 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
-@RequestMapping("/categories")
+@RequestMapping("/color")
 @RestController
-public class CategoriesController {
+public class ClolorController {
     @Autowired
-    CategoriesService categoriesSrevice;
+    private ColorServices colorServices;
 
     @GetMapping("/getall")
-    public Response<List<Categories>> getAll() {
-        return Response.ofSucceeded(categoriesSrevice.getAllCategories());
+    public Response<List<Colors>> getAll() {
+        return Response.ofSucceeded(colorServices.getAllColor());
     }
 
     @PostMapping("/add")
-    public Response<Categories> create(@RequestBody Categories categories) {
+    public Response<Colors> create(@RequestBody Colors color) {
         try {
-           Categories categorie = categoriesSrevice.saveCategories(categories);
-            return Response.ofSucceeded(categorie);
+            Colors colors = colorServices.saveColor(color);
+            return Response.ofSucceeded(colors);
         } catch (IllegalArgumentException e) {
             return Response.ofError(e.getMessage());
         }
     }
 
     @PutMapping("/update/{id}")
-    public Response<Categories> update(@PathVariable Long id, @RequestBody Categories categories) {
+    public Response<Colors> update(@PathVariable Long id, @RequestBody Colors color) {
         try {
-            categories.setId(id);
-            Categories categorie = categoriesSrevice.saveCategories(categories);
-            return Response.ofSucceeded(categorie);
+            color.setId(id);
+            Colors colors = colorServices.saveColor(color);
+            return Response.ofSucceeded(colors);
         } catch (IllegalArgumentException e) {
             return Response.ofError(e.getMessage());
         }
@@ -48,20 +50,20 @@ public class CategoriesController {
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> delete(@PathVariable Long id) {
+
         try {
-            categoriesSrevice.deleteCategoriesById(id);
-            return ResponseEntity.ok("Loại đã được xóa thành công");
+            colorServices.deleteColorById(id);
+            return ResponseEntity.ok("Màu sắc đã được xóa thành công");
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
-
 
     }
 
     @GetMapping("/get-page")
     public List<?> getPage(@RequestParam(defaultValue = "0") int p) {
         Pageable pageable = PageRequest.of(p, 5);
-        Page<Categories> page = categoriesSrevice.findAllCategories(pageable);
+        Page<Colors> page = colorServices.findAllColor(pageable);
         return page.toList();
     }
 
@@ -69,12 +71,11 @@ public class CategoriesController {
     public Response<List<Long>> deleteMultiple(@RequestBody Map<String, List<Long>> request) {
         List<Long> ids = request.get("id");
         try {
-            categoriesSrevice.deleteCategoriesByIds(ids);
+            colorServices.deleteColorsByIds(ids);
             return Response.ofSucceeded();
         } catch (IllegalArgumentException e) {
             return Response.ofError(e.getMessage());
         }
+
     }
-
-
 }
