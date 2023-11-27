7e9a1633566cbc1edf012cda40821fac67f5747a
Thuphuong03 <phuongbttph20543@fpt.edu.vn>
2023-11-28 03:39:48 +0700

    thuphuong

--- src/main/java/com/example/sd_95_polo_store_be/Service/OrderService.java
+++ src/main/java/com/example/sd_95_polo_store_be/Service/OrderService.java
@@ -1,9 +1,11 @@
 package com.example.sd_95_polo_store_be.Service;
 import com.example.sd_95_polo_store_be.Model.Entity.Oders;
+import com.example.sd_95_polo_store_be.Model.Request.ChangeStatusOrder;
 
 import java.util.List;
 
 public interface OrderService {
     List<Oders> getByCustomer(Integer id);
     List<Oders> getAll();
+    void updateStatusOrder(Integer id, ChangeStatusOrder changeStatusOrder);
 }
