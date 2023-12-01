0438774bdf5377ae8a6b7cd1cf9e46ec75615731
huong03 <phamquanghuong03@gmail.com>
2023-12-02 00:38:50 +0700

    huong03

--- src/main/java/com/example/sd_95_polo_store_be/Service/OrderService.java
+++ src/main/java/com/example/sd_95_polo_store_be/Service/OrderService.java
@@ -2,6 +2,7 @@ package com.example.sd_95_polo_store_be.Service;
 import com.example.sd_95_polo_store_be.Model.Entity.Oders;
 import com.example.sd_95_polo_store_be.Model.Request.ChangeStatusOrder;
 import com.example.sd_95_polo_store_be.Model.Request.OrderRequest;
+import com.example.sd_95_polo_store_be.Model.Response.OrderResponse;
 import com.example.sd_95_polo_store_be.Model.Response.OrderVnpayResponse;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public interface OrderService {
     void updateStatusOrder(Integer id, ChangeStatusOrder changeStatusOrder);
     OrderVnpayResponse orderOnline(OrderRequest orderRequest, Integer id);
     Oders get(Integer id);
-
     void changeTransaction(Integer id);
+
+    OrderResponse getOneOrder(Integer id);
 }
