9bbe4a5e97a07d854cb483e312b675c2aacf370a
zluong24 <zluong24@gmail.com>
2023-12-21 01:37:21 +0700

    update

--- src/main/java/com/example/sd_95_polo_store_be/Service/RoleService.java
+++ src/main/java/com/example/sd_95_polo_store_be/Service/RoleService.java
@@ -1,5 +1,6 @@
 package com.example.sd_95_polo_store_be.Service;
 
+import com.example.sd_95_polo_store_be.Model.Entity.Colors;
 import com.example.sd_95_polo_store_be.Model.Entity.Role;
 
 import java.util.List;
@@ -7,4 +8,14 @@ import java.util.List;
 public interface RoleService {
 
     List<Role> getAll();
+
+    List<Role> getColorByStatus();
+
+    Role saveRole(Role role);
+
+    Role getOne(Integer id);
+
+    Role findById(Integer id);
+    public void changeStatus(Integer id);
+    boolean isColorDataDuplicate(Role role);
 }
